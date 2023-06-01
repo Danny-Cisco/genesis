72f99826813b0d531c0c1fdc295d227e952b12e2
Danny-Cisco <107174335+Danny-Cisco@users.noreply.github.com>
2023-06-02 00:18:16 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 /* global rive */
-
 var buttons = document.getElementsByClassName("changeColorButton");
 var headings = document.getElementsByClassName("heading");
 
 for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", function () {
-    var currentColor = headings[i].style.color;
-
-    if (currentColor === "pink") {
-      headings[i].style.color = "blue";
-      headings[i].style.fontSize = "15px"; // original font size
-    } else {
-      headings[i].style.color = "pink";
-      headings[i].style.fontSize = "25px"; // slightly larger font size
-    }
+  buttons[i].addEventListener("click", function() {
+    headings[i].classList.toggle("pink");
   });
 }
+
